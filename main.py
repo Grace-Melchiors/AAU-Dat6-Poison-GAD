5042ec5b6425555a8ba0691fdb3b3f9416c833c6
Frederik-Melchiors <fredmelchiors@gmail.com>
2024-03-08 14:06:34 +0100

    small utils, and moved to main

--- main.py
+++ main.py
@@ -14,34 +14,64 @@ A main file which calls these as needed
 """
 
 
-#Controls, select dataset, anomaly injection, poison, run methods, and display options
+from torch_geometric.datasets import Planetoid
+import torch_geometric.utils as pyg_utils
+import networkx as nx
+import matplotlib.pyplot as plt
+import torch
+
+import torch_geometric.transforms as T
 
 
 #Import from different folders with stuff
 
 
 
+#Controls, select dataset, anomaly injection, poison, run methods, and display options
+
+DATASET_NAME = 'Cora'  #Options: 'Cora', 
 
 
-#LoadDataset
+if __name__ == '__main__':
+### LoadDataset ###
+    # the [0] at the end is used to access the attributes
+    data = Planetoid('./data/'+DATASET_NAME, DATASET_NAME, transform=T.NormalizeFeatures())[0]
+    dataset_name = 'Cora'
+
+    # Load the dataset
+    dataset = Planetoid(root='./data', name=DATASET_NAME)
+
+    # Access the dataset attributes
+    data = dataset[0]
+
+    # Convert PyTorch Geometric data to NetworkX graph ---
+    base_graph_dataset = pyg_utils.to_networkx(data, to_undirected=True)
 
 
 
+### Inject anomalies in dataset ###
+    from pygod.generator import gen_contextual_outlier, gen_structural_outlier
 
-#Inject anomalies in dataset
+    # introduces anomalies (maybe poison I can't tell) to the train data in case the original dataset didnt have any
+    data, ya = gen_contextual_outlier(data, n=100, k=50) #In
+    data, ys = gen_structural_outlier(data, m=10, n=10)
+    data.y = torch.logical_or(ys, ya).long()
 
-
-#Poison dataset
-
-
-#Run model(s)
-
-
-# Compare clean model with poison or/and get performance at different amounts of poison
-
+    # Convert PyTorch Geometric data to NetworkX graph ---
+    clean_graph_dataset = pyg_utils.to_networkx(data, to_undirected=True)
 
 
-#Produce results
+### Poison dataset ###
+
+
+### Run model(s) ###
+
+
+### Compare clean model with poison or/and get performance at different amounts of poison ###
+
+
+
+### Produce results ###
 
 
 
