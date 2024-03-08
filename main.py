97775318805e8e8db3c257c8218825e2d768616d
Frederik-Melchiors <fredmelchiors@gmail.com>
2024-03-08 11:38:04 +0100

    Base layout for main

---
+++ main.py
@@ -0,0 +1,56 @@
+"""
+# Structure
+A folder for datasets, which includes original datasets, with anomalies, and poisoned version
+
+A folder for anomaly injection
+
+A folder for poisons
+
+A folder for GADs
+
+A folder for results, includes raw data, and graphs
+
+A main file which calls these as needed
+"""
+
+
+#Controls, select dataset, anomaly injection, poison, run methods, and display options
+
+
+#Import from different folders with stuff
+
+
+
+
+
+#LoadDataset
+
+
+
+
+#Inject anomalies in dataset
+
+
+#Poison dataset
+
+
+#Run model(s)
+
+
+# Compare clean model with poison or/and get performance at different amounts of poison
+
+
+
+#Produce results
+
+
+
+
+
+
+
+
+
+
+
+
