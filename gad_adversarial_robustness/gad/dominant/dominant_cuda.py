3fb2478d689c26313fe3facc1d95d721203f9659
Karen A <48069766+kaandersen@users.noreply.github.com>
2024-04-28 19:50:34 +0200

    detatch().cpu().numpy() removed from self.label

--- gad_adversarial_robustness/gad/dominant/dominant_cuda.py
+++ gad_adversarial_robustness/gad/dominant/dominant_cuda.py
@@ -101,7 +101,8 @@ class Dominant(nn.Module):
                 A_hat, X_hat = self.forward(self.attrs, self.edge_index)
                 loss, struct_loss, feat_loss = loss_func(self.adj_label, A_hat, self.attrs, X_hat, config['model']['alpha'])
                 self.score = loss.detach().cpu().numpy()
-                print(f"Epoch: {epoch:04d}, Auc: {roc_auc_score(self.label.detach().cpu().numpy(), self.score)}")
+                # print(f"Epoch: {epoch:04d}, Auc: {roc_auc_score(self.label.detach().cpu().numpy(), self.score)}")
+                print(f"Epoch: {epoch:04d}, Auc: {roc_auc_score(self.label, self.score)}")
 
                 # Identify and store the IDs of the nodes with the top K highest anomaly scores
                 if top_k is not None:
